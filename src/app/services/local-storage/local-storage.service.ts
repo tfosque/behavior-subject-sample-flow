b61205b9b0ed4114dca96f91f863742b09e3be9c
Tim Fosque <tfosque@gmail.com>
2020-07-15 10:43:51 -0400

    wipi: switch computers

--- src/app/services/local-storage/local-storage.service.ts
+++ src/app/services/local-storage/local-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { size } from 'lodash';
 
@@ -27,41 +27,17 @@ export class LocalStorageService {
   private readonly apiBaseUrl = 'https://my.api.mockaroo.com/cartitem_schema.json?key=c1a35bd0';
 
   public LOC_DATA = new BehaviorSubject<any>([]);
-  public currStorage = JSON.parse(localStorage.getItem('localDb'));
+  public currStorage = JSON.parse(localStorage.getItem('products'));
+
+  public products = JSON.parse(localStorage.getItem('products'));
+  public shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
 
   constructor(private readonly http: HttpClient) { }
-
-  /*   configDb() {
-      const stor = JSON.parse(localStorage.getItem('localDbConfig'));
-      const configValue = stor.configured;
-      console.log({ configValue });
-
-      if (configValue) {
-        return;
-      }
-
-      // set config
-      let localDbConfig: Config;
-
-      const storagetype = 'local';
-      const datatype = 'json';
-      const msg = 'Welcome to localDb (Fake Data Persistence)';
-      const configured = true;
-
-      localDbConfig = { storagetype, datatype, msg, configured };
-      // this.setConfiguration(localDbConfig);
-      return localDbConfig;
-    } */
-
-  // Configuration
-setConfiguration(localDbConfig: any): any {
-    localStorage.setItem('localDbConfig', JSON.stringify(localDbConfig));
-  }
 
   /**
    * Check Local Storage to see if data exist (and if storage space is available 5MB max)
    */
-startLocalDb(): void {
+  startLocalDb(): void {
     // Scan for localDb..
     if (this.currStorage) {
       console.log('localDb is live!', this.currStorage);
@@ -73,34 +49,42 @@ startLocalDb(): void {
       return;
     }
     if (!this.currStorage) {
-          console.log('No localDb found');
-          this.initLocalDb();
-          return;
-        }
+      console.log('No localDb found');
+      this.initLocalProductsDb().subscribe(sub => {
+        this.initLocalShoppingCartDb();
+      });
+
+      return;
+    }
 
     console.group('Application Logs');
     console.groupEnd();
   }
 
-localDbSize(): any {
+  localDbSize(): any {
     // this will produce an error if localDb does not exist
     const storage = this.currStorage;
     const chkSize = size(storage);
     return chkSize;
   }
 
-initLocalDb() {
+  initLocalProductsDb(): Observable<any> {
     this.http.get(this.apiBaseUrl).subscribe((data) => {
       // console.log({ data });
-      this.LOC_DATA.next(data);
-      localStorage.setItem('localDb', JSON.stringify(data));
+      // this.LOC_DATA.next(data);
+      localStorage.setItem('products', JSON.stringify(data));
+      // return data;
     });
+    return;
+  }
+  initLocalShoppingCartDb() {
+    localStorage.setItem('shooppingCart', JSON.stringify([]));
   }
 
   /**
    * Returns the total amount of disk space used (in MB) by localStorage for the current domain.
    */
-getLocalStorage() {
+  getLocalStorage() {
     let total = 0;
     // tslint:disable-next-line: forin
     for (let x in localStorage) {
@@ -113,8 +97,4 @@ getLocalStorage() {
     return total.toFixed(2);
   }
 
-  /*  emptyDbAction(exit: boolean) {
-     if db is empty send developer a message to continue with empty data or fetch fresh data;
-     const msg = prompt(`Your localDb size is - ${this.localDbSize()}`, 'Hello');
-   } */
 }
